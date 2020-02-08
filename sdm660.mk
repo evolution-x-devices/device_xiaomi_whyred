3793be129d46a65f48ab9ce24a356f3ac98ad51d
HeartStealer786 <beingme639@gmail.com>
2020-02-09 02:40:28 +0530

    Revert "sdm660-common: Add back MiWatermark lib and remove shim"
    This reverts commit 63d1420d9019eb82d40d156ad0b00fcf58f8e21b.

--- sdm660.mk
+++ sdm660.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     org.ifaa.android.manager
 
+# Camera
+PRODUCT_PACKAGES += \
+    libMiWatermark_shim
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service
