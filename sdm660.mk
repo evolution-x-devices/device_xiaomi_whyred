c9db05571466ad8df687b31bfb3d5eaeab2f68c8
HeartStealer786 <beingme639@gmail.com>
2020-02-09 02:39:57 +0530

    decommonize parts

--- sdm660.mk
+++ sdm660.mk
@@ -178,9 +178,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Device-specific Settings
-PRODUCT_PACKAGES += \
-    XiaomiParts
+
 
 # Display
 PRODUCT_PACKAGES += \
