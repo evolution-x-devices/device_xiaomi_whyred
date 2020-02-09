167d65b249edbe030a9755732173265f46357e60
HeartStealer786 <beingme639@gmail.com>
2020-02-09 15:45:55 +0530

    whyred : Enable USB debugging at boot

--- properties.mk
+++ properties.mk
@@ -3,3 +3,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.fm.a2dp.conc.disabled=true \
     vendor.audio.feature.fm.enable=true \
     vendor.hw.fm.init=0
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
