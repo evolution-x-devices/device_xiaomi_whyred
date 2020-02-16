683da11504dfc7e8998bfdd79b9a5cc925a17dc9
HeartStealer786 <beingme639@gmail.com>
2020-02-16 23:14:50 +0530

    evo-ify

--- aosp_whyred.mk
+++ aosp_whyred.mk
@@ -20,19 +20,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common aosip stuff
-$(call inherit-product, vendor/stag/main.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from whyred device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 $(call inherit-product, vendor/MiuiCamera/config.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+CUSTOM_BUILD_TYPE := REBIRTH
 
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := stag_whyred
+PRODUCT_NAME := aosp_whyred
 PRODUCT_MODEL := Redmi Note 5 Pro
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
